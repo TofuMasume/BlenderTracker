1e1104101ba2c693ed0e20b840d97fda31237c7c
Tofu Masume <tofu.masume@gmail.com>
2026-02-24 17:22:37 +0900

    feat: add addon entry point with bl_info metadata

---
+++ __init__.py
@@ -0,0 +1,21 @@
+bl_info = {
+    "name": "Pattern & Version Manager",
+    "author": "masume",
+    "version": (0, 1, 0),
+    "blender": (4, 0, 0),
+    "location": "View3D > N-Panel > Pt/Ver",
+    "description": "Manage object naming with Pt.NNN / v.NNN suffixes",
+    "category": "Object",
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == "__main__":
+    register()
